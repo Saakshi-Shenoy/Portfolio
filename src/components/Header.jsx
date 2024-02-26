0404e3938b6e559c1f6eb534e233d223bcc2524f
Saakshi-Shenoy <shenoy.saakshi910@gmail.com>
2024-02-26 18:29:59 +0530

    projects-done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 
 function Header() {
 
-  const handleNavItemClick = (itemName) => {
-    const element = document.getElementById(itemName);
-    if(element) {
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  };
+  // const handleNavItemClick = (itemName) => {
+  //   const element = document.getElementById(itemName);
+  //   if(element) {
+  //     element.scrollIntoView({ behavior: "smooth" });
+  //   }
+  // };
 
   return (
     <div style={{ fontFamily: "Montserrat" }}>
@@ -23,34 +23,34 @@ function Header() {
           <Navbar.Toggle />
         </div>
         <Navbar.Collapse className="ml-auto mr-10">
-          <button className="font-bold duration-500 ease-in-out hover:scale-110 active:animate-bounce">
+          <button className="font-bold duration-500 ease-in-out hover:scale-110">
             <Navbar.Link
               as={Link}
               to="/about"
               className="text-xl font-semibold text-white lg:mt-2 md:mt-2 hover:text-teal-800 md:hover:text-white focus:underline mb-1"
-              onClick={() => handleNavItemClick("about")}
+              // onClick={() => handleNavItemClick("about")}
             >
               About
             </Navbar.Link>
           </button>
 
-          <button className="font-bold duration-500 ease-in-out hover:scale-110 active:animate-bounce">
+          <button className="font-bold duration-500 ease-in-out hover:scale-110">
             <Navbar.Link
               as={Link}
               to="/projects"
               className="text-xl font-semibold text-white lg:mt-2 md:mt-2 hover:text-teal-800 md:hover:text-white focus:underline mb-1"
-              onClick={() => handleNavItemClick("projects")}
+              // onClick={() => handleNavItemClick("projects")}
             >
               Projects
             </Navbar.Link>
           </button>
 
-          <button className="font-bold duration-500 ease-in-out hover:scale-110 active:animate-bounce">
+          <button className="font-bold duration-500 ease-in-out hover:scale-110">
             <Navbar.Link
               as={Link}
               to="/experience"
               className="text-xl font-semibold text-white lg:mt-2 md:mt-2 hover:text-teal-800 md:hover:text-white focus:underline mb-1"
-              onClick={() => handleNavItemClick("experience")}
+              // onClick={() => handleNavItemClick("experience")}
             >
               Experience
             </Navbar.Link>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,82 +1,86 @@
 import React from 'react';
+import Jiffy from "../assets/jiffy.png";
+import Newsmon from "../assets/newsmon.png";
+import Catalysis from "../assets/catalysis.png";
+import SmartSpend from "../assets/smartspend.png"
 
 function Projects() {
   return (
-    <div className='bg-teal-800'>
+    <div className='bg-white bg-cover'>
     <section className="text-gray-600 body-font" style={{ fontFamily: "Montserrat"}}>
     <div className="container px-5 py-10 mx-auto">
-          <h1 className="sm:text-3xl text-3xl text-white font-bold title-font mb-10 text-gray-900 items-center text-center justify-center" style={{fontSize:"2.5em"}}>
+          <h1 className="sm:text-3xl text-3xl text-teal-800 font-bold title-font mb-12 mt-5 text-gray-900 items-center text-center justify-center" style={{fontSize:"2.5em", fontWeight: "730"}}>
             PROJECTS
           </h1>
-      <div className="flex flex-wrap -m-4">
+      <div className="flex flex-wrap -m-4 p-4">
         <div className="xl:w-1/4 md:w-1/2 p-4">
-          <div className="bg-gray-100 p-6 rounded-lg">
+          <div className="bg-teal-800 p-6 rounded-lg">
             <img
               className="h-40 rounded w-full object-cover object-center mb-6"
-              src="https://dummyimage.com/720x400"
+              src={Newsmon}
               alt="content"
             />
-            <h3 className="tracking-widest text-xs font-bold title-font mb-2">
+            <h3 className="tracking-widest text-xs font-bold title-font mb-2 text-white">
               FRONTEND
             </h3>
-            <h2 className="text-lg text-gray-900 font-bold title-font mb-3">
+            <h2 className="text-lg text-white font-bold title-font mb-3">
               NEWSMON
             </h2>
-            <p className="leading-relaxed text-sm" style={{fontSize: "0.9em"}}>
+            <p className="leading-relaxed text-sm text-white" style={{fontSize: "0.9em"}}>
               A fully responsive website that provides timely coverage and other information about important events around the globe through the usage of APIs.
             </p>
           </div>
         </div>
         <div className="xl:w-1/4 md:w-1/2 p-4">
-          <div className="bg-gray-100 p-6 rounded-lg">
+          <div className="bg-teal-800 p-6 rounded-lg">
             <img
               className="h-40 rounded w-full object-cover object-center mb-6"
-              src="https://dummyimage.com/721x401"
+              src={Jiffy}
               alt="content"
             />
-            <h3 className="tracking-widest text-500 text-xs font-bold title-font mb-2">
+            <h3 className="tracking-widest text-500 text-xs font-bold title-font mb-2 text-white">
               MERN STACK
             </h3>
-            <h2 className="text-lg text-gray-900 font-bold title-font mb-3">
+            <h2 className="text-lg text-white font-bold title-font mb-3">
               JIFFY
             </h2>
-            <p className="leading-relaxed text-base" style={{fontSize: "0.9em"}}>
+            <p className="leading-relaxed text-base text-white" style={{fontSize: "0.9em"}}>
               A responsive and robust food ordering and delivery website designed to streamline and expedite the food delivery process.
             </p>
           </div>
         </div>
         <div className="xl:w-1/4 md:w-1/2 p-4">
-          <div className="bg-gray-100 p-6 rounded-lg">
+          <div className="bg-teal-800 p-6 rounded-lg">
             <img
               className="h-40 rounded w-full object-cover object-center mb-6"
-              src="https://dummyimage.com/722x402"
+              src={SmartSpend}
               alt="content"
             />
-            <h3 className="tracking-widest mb-2 text-xs font-bold title-font">
+            <h3 className="tracking-widest mb-2 text-xs font-bold title-font text-white">
               FERN STACK
             </h3>
-            <h2 className="text-lg text-gray-900 font-bold title-font mb-3">
+            <h2 className="text-lg font-bold title-font mb-3 text-white">
               SMARTSPEND
             </h2>
-            <p className="leading-relaxed text-base" style={{fontSize: "0.9em"}}>
+            <p className="leading-relaxed text-base text-white" style={{fontSize: "0.9em"}}>
               An expense tracking website that includes the features of budget management, financial insights, peer-to-peer lending and reminders.
             </p>
           </div>
         </div>
         <div className="xl:w-1/4 md:w-1/2 p-4">
-          <div className="bg-gray-100 p-6 rounded-lg">
+          <div className="bg-teal-800 p-6 rounded-lg">
             <img
               className="h-40 rounded w-full object-cover object-center mb-6"
-              src="https://dummyimage.com/723x403"
+              src={Catalysis}
               alt="content"
             />
-            <h3 className="tracking-widest text-xs font-bold title-font mb-2">
+            <h3 className="tracking-widest text-xs font-bold title-font mb-2 text-white">
               MERN STACK
             </h3>
-            <h2 className="text-lg text-gray-900 font-bold title-font mb-3">
+            <h2 className="text-lg text-white font-bold title-font mb-3">
               CATALYSIS v2.0
             </h2>
-            <p className="leading-relaxed text-base" style={{fontSize: "0.9em"}}>
+            <p className="leading-relaxed text-base text-white" style={{fontSize: "0.9em"}}>
               Contribution to a college event website for a Technical fest that provides details regarding the events and facilitates the feature of registration to the events.
             </p>
           </div>
